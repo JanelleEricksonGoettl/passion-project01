37226ee1a10b8ccb7becc8644c035f958d4408a9
Janelle Erickson-Goettl <janelle.erickson.goettl@gmail.com>
2019-02-25 16:44:32 -0500

    created events model and ran db migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   get 'events/index'
-  get 'pages/home'
+
+  resources :events
 
   root "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
