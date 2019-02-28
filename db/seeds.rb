b3093c5c56ce474f52f2570d54334a20693cfec4
Janelle Erickson-Goettl <janelle.erickson.goettl@gmail.com>
2019-02-28 13:00:19 -0500

    added event seed data in chronological order

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,47 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Event.create(
-  title: "Sad Poetry Hour",
-  date: ,
-  event_description: "Bring your saddest sad poetry to Daily Press Coffee at 38 Somers for Sad Poetry Hour starting at 7pm.",
-  image_url: ""
+  title: "Genre Reassignment - Jes Tom",
+  date: DateTime.parse('4th Sep 2018'),
+  event_description: "Jes Tom (they/them) is an actor, writer, and weird queer stand up comic. Born & raised in San Francisco and now based in New York, Jes's comedy gleefully provides the nonbinary queer Asian American radical cyborg perspective that everyone never knew they wanted. Jes has shared stages with Awkwafina, Kate Bornstein, Aparna Nancherla, and Rosie O'Donnell. Their writing has been published by Reductress, By Shondaland, and Condé Nast's THEM. Jes has been featured in GO Magazine, Splitsider, and the Fader, and were named one of TimeOut New York's 'LGBTQ POC Comedians We're Obsessed With.' This year they were named a StandUp NBC semifinalist. Follow Jes on Twitter @jestom and Instagram @jesthekid",
+  image_url: "/images/events/2018-sept.jpg"
 )
 
 Event.create(
-  title: "",
-  date: ,
-  event_description: "",
-  image_url: ""
+  title: "Genre Reassignment - Wren Hanks",
+  date: DateTime.parse('2nd Oct 2018'),
+  event_description: "Wren Hanks is the author of The Rise of Genderqueer (Brain Mill Press) and Prophet Fever (Hyacinth Girl Press) and co-editor of Curious Specimens (Sundress Publications), an anthology of the strange and uncanny. A 2016 Lambda Emerging Writers Fellow, his work appears or is forthcoming in Best New Poets, Foglifter, Gigantic Sequins, Waxwing, and elsewhere. He lives in Brooklyn with his wife and tweets @suitofscales.",
+  image_url: "/images/events/2018-oct.jpg"
 )
 
 Event.create(
-  title: "",
-  date: ,
-  event_description: "",
-  image_url: ""
+  title: "Genre Reassignment - Bishakh Som",
+  date: DateTime.parse('6th Nov 2018'),
+  event_description: "Bishakh Som is writer and illustrator whose work has appeared in The New Yorker, We're Still Here (The first all-trans comics anthology), The Other Side: An Anthology of Queer Paranormal Romance Comics, Beyond, vol. 2 (The Queer Post-Apocalyptic & Urban Fantasy Comics Anthology), The Strumpet, The Boston Review, Black Warrior Review, VICE, The Brooklyn Rail, Buzzfeed, Ink Brick, The Huffington Post, The Graphic Canon vol. 3 and Little Nemo: Dream Another Dream. She received the Xeric grant in 2003 for her comics collection Angel.",
+  image_url: "/images/events/2018-nov.jpg"
+)
+
+Event.create(
+  title: "Genre Reassignment - Nahshon Anderson",
+  date: DateTime.parse('4th Dec 2018'),
+  event_description: "Nahshon Anderson is a writer and emerging screenwriter from Altadena, California.
+
+  They’re a contributing writer to Lambda Literary Review, a grants advisory panelist for the New York State Council on The Arts, and have previously served as Scholastic Art & Writing Awards and Lambda Literary juror.
+  
+  Nahshon has recently completed their courageous and gut-wrenching memoir, Shooting Range which is currently being shopped around for a publishing deal.",
+  image_url: "/images/events/2018-dec.jpg"
+)
+
+Event.create(
+  title: "Genre Reassignment - Kit Yan",
+  date: DateTime.parse('8th Jan 2019'),
+  event_description: "Kit Yan is a poet, lyricist, and playwright whose work has been produced by the American Repertory Theater, the Smithsonian, Musical Theater Factory, the New York Musical Festival, Diversionary Theater, and Dixon Place. They have been a resident with the Civilians, Mitten Lab, 5th Avenue Theater, the Village Theater, a WP/Public theater translab fellow, and are a 2018-19 Dramatists Guild Foundation Fellow and Lincoln Center Writer in residence.",
+  image_url: "/images/events/2019-jan.jpg"
+)
+
+Event.create(
+  title: "Genre Reassignment - Jeanne Thornton",
+  date: DateTime.parse('12th Feb 2019'),
+  event_description: "Jeanne Thornton is a Lambda award finalist for her occult novel, 'The Dream of Dr. Bantam,' as well as for her short story collection, 'The Black Emerald.' Her fiction has appeared in n+1, WIRED, the Evergreen Review, WSQ, and other places. She is the copublisher of Instar Books and creator of comic strips 'Bad Mother' and 'The Man Who Hates Fun'",
+  image_url: "/images/events/2019-feb.jpg"
 )
